a91944bcb1c80ede772465b278593bcd07f1c473
Skylar Ennenga <skylar.ennenga@outlook.com>
2024-07-25 12:19:07 -0600

    Fixed Comments

--- Lesson 4 Assignments JavaScript Objects.js
+++ Lesson 4 Assignments JavaScript Objects.js
@@ -12,18 +12,6 @@
 
 // Task 4: Create functions that utilize the filter method to filter out books that contain more than 100 pages and the map method to add "Title: " and "Author: " to the book's title and author properties respectably.
 
-// Expected Outcomes:
-
-//     After completing this assignment, students should be able to create objects using constructor functions in JavaScript.
-
-//     Students should be proficient in adding properties and methods to objects using prototypes.
-
-//     Students should be able to manipulate arrays of objects and implement functions to perform various operations on object data.
-
-
-
-// Task 1: Create a constructor function for the Book object with properties for title, author, and pages.
-
 // Constructor for book object
 function  Book(title, author, pages,){
     this.title = title; 
@@ -124,28 +112,6 @@ function filterBookPages() {
 
 // Task 3: Create a method to calculate compound interest based on the balance and specified interest rate. 
 // Allow users to pass a year parameter to your method that represents the # of years the money has been invested and an interest rate. The output should be rounded up to the nearest whole number (perhaps using the Math.ceil()). The formula for compound interest is...
-
-// A=P(1+nr​)^nt
-
-// Where:
-
-//     𝐴 is the amount of money accumulated after n years, including interest.
-
-//     𝑃 is the principal amount (the initial sum of money).
-
-//     𝑟 is the annual interest rate (in decimal form).
-
-//     𝑛 is the number of times interest is compounded per year (you can use 1 for this example).
-
-//     𝑡 is the number of years the money is invested or borrowed for (this will be your parameter passed into the method).
-
-// Expected Outcomes:
-
-//     After completing this assignment, students should be able to create objects using constructor functions and manipulate their properties and methods in JavaScript.
-
-//     Students should understand how to perform basic mathematical operations and calculations using the Math class.
-
-//     Students should be able to apply object-oriented principles and mathematical concepts to solve real-world problems, such as financial calculations in a banking application.
 
 
 // Constructor for book object
